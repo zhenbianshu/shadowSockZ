f1880765480d222f152010df0d607104fa07dd39
zhenbianshu <zhenbianshu@foxmail.com>
2019-01-22 21:09:23 +0800

    add gradle deploy method;

--- client/src/main/java/com/zhenbianshu/shadowsockz/run/Bootstrap.java
+++ client/src/main/java/com/zhenbianshu/shadowsockz/run/Bootstrap.java
@@ -1,10 +1,11 @@
 package com.zhenbianshu.shadowsockz.run;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -12,8 +13,12 @@ import org.springframework.context.annotation.ImportResource;
  * created by zbs on 2018/11/29
  */
 @Configuration
+@SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class,
         RedisRepositoriesAutoConfiguration.class})
 @ImportResource({"classpath:spring/client.xml"})
-public class Bootstrap extends SpringBootServletInitializer {
+public class Bootstrap {
+    static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
 }
